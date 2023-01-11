cb5fae88acff9efac1e09386f9aae5e1b6651e59
unknown <muhacolee4@gmail.com>
2023-01-11 10:04:15 +0000

    ox module

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
 import this
+import antigravity
+
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for char in range(26):
-    if char != 4 and char != 16:
+    # if char != 4 and char != 16:
         print("{:s}".format(chr(char + ord("a"))), end="")
+print(ord('a'))
+print(chr(122))
